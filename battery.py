b91bf95f9e5e37a7fcc1926bf64e5c27b6494219
achrrr <111825486+achrrr@users.noreply.github.com>
2023-03-05 02:26:06 +0000

    Update battery.py

--- battery.py
+++ battery.py
@@ -2,38 +2,38 @@ from abc import ABC, abstractmethod
 from datetime import datetime
 
 class Battery(ABC):
-
-    def __init__(self, last_service_date:datetime):
-
-        self.last_service_date = last_service_date
-
-
     @abstractmethod
     def battery_needs_service(self):
         pass
 
 class SpindlerBattery(Battery):
 
-    def __init__(self, last_service_date:datetime) -> None:
-        super().__init__(last_service_date)
+    def __init__(self, current_date, last_service_date:datetime) -> None:
 
+        self.current_date = current_date
         self.last_service_date = last_service_date
 
-    def battery_needs_service(self):
+    def battery_needs_service(self) -> bool:
         
         threshold_date:datetime = self.last_service_date.replace(year = self.last_service_date.year + 3)
 
-        return True if threshold_date < datetime.today().date() else False
-
+        if threshold_date < self.current_date:
+            return True
+        else:
+            return False
 
 class NubbinBattery(Battery):
 
-    def __init__(self, last_service_date: datetime):
-        super().__init__(last_service_date)
+    def __init__(self, current_date, last_service_date: datetime):
         
+        self.current_date = current_date
         self.last_service_date = last_service_date
 
-    def battery_needs_service(self):
+    def battery_needs_service(self) -> bool:
         threshold_date:datetime =  self.last_service_date.replace(year = self.last_service_date.year + 4)
 
-        return True if threshold_date < datetime.today().date() else False
+        if threshold_date < self.current_date:
+            return True
+        else:
+            return False
+
